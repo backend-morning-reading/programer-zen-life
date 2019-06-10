7d6b2c46e412f320902071bc5b1f7b0f18902206
chenjinya <jinyachen@gmail.com>
2019-06-10 19:36:52 +0800

    first

---
+++ scripts/cli.js
@@ -0,0 +1,51 @@
+#!node
+
+const fs = require('fs');
+const _ = require("lodash")
+const jsChapterFilePath = './scripts/chapters.js';
+const replaceFlag = "//--CHAPTER_ADD_HERE--DO_NOT_MODIFY_THIS_LINE--//";
+
+let _i = _.values(process.argv);
+_i.shift();
+_i.shift();
+let cmd_input = _i;
+
+if (0 === cmd_input.length % 2) {
+  console.error(`❌ Generate fail!  第一个参数为章节文件名，以后的参数为: 章节选项文案 章节编号,多个章节选项以此类推`);
+  process.exit(0);
+}
+
+
+let chapter_file_name = cmd_input[0];
+
+let chapter_options = [];
+for (let i = 1; cmd_input[i]; i += 2) {
+  chapter_options.push([cmd_input[i], cmd_input[i + 1]]);
+}
+
+chapter_name = chapter_file_name.replace(".md", '');
+chapter_name = chapter_name.replace(/ \,\'\"\./g, '-');
+
+
+let fileContent = fs.readFileSync(`./chapters/${chapter_file_name}`, {
+  encoding: 'utf8'
+});
+let convContent = {}
+let chapterBody = [
+  [fileContent]
+];
+for (let o of chapter_options) {
+  chapterBody.push(o);
+}
+
+convContent = `${chapter_name}: ` + JSON.stringify(chapterBody);
+
+let jsFileContent = fs.readFileSync(jsChapterFilePath, {
+  encoding: 'utf8'
+});
+
+jsFileContent = jsFileContent.replace(replaceFlag, `${convContent},\n${replaceFlag}`);
+
+fs.writeFileSync(jsChapterFilePath, jsFileContent);
+console.log(`✅ Generate success! ${chapter_name}`);
+// console.log(convContent);
---
+++ scripts/main.js
@@ -0,0 +1,52 @@
+const markdownConv = new showdown.Converter();
+const formatChapter = (_chapter) => {
+    const chapter = [].concat(_chapter);
+    const content = markdownConv.makeHtml(chapter[0].join("\n\n"));
+
+    delete (chapter[0]);
+    let options = [];
+    for (let i in chapter) {
+        let item = chapter[i];
+        options.push({
+            text: item[0],
+            next: item[1],
+            action: item[2] ? item[2] : null
+        })
+    }
+    return {
+        content,
+        options
+    }
+}
+(function () {
+    const vm = new Vue({
+        el: '#app',
+        data: function () {
+            return {
+                title: "程序员修仙传",
+                author: "听雨",
+                chapter_index: 0,
+                chapter: formatChapter(chapters[0]),
+            }
+        },
+        mounted: function () {
+            document.title = this.title;
+            document.getElementById('app').style.height = window.innerHeight + "px";
+            if (window.location.hash) {
+                this.chapter_index = window.location.hash.replace('#', '');
+                if (!chapters[this.chapter_index]) {
+                    this.chapter_index = 0;
+                }
+            }
+            this.chapter = formatChapter(chapters[this.chapter_index]);
+
+        },
+        methods: {
+            next(n) {
+                this.chapter_index = n;
+                window.location.hash = '#' + n;
+                this.chapter = formatChapter(chapters[n])
+            }
+        }
+    });
+})()
