0e472a497d97608d9ae6444529d1c542f668a468
jinya <jinyachen@gmail.com>
2019-06-12 22:43:35 +0800

    up: gen cli

--- scripts/generator.js
+++ scripts/generator.js
@@ -3,9 +3,12 @@
 //生成章节
 
 const fs = require('fs');
+const jsChapterFilePath = __dirname +'/chapters.js';
+const jsChapterJSONPath = __dirname +'/chapters.json';
+const chapters = require(jsChapterJSONPath);
+
 const generateChapter = (cmd_input) => {
-  const jsChapterFilePath = './scripts/chapters.js';
-  const jsChapterJSONPath = './scripts/chapters.json';
+  
   const autoIntent = true;
   if (!cmd_input || 0 === cmd_input.length % 2) {
     console.error(`❌ Generate fail!  第一个参数为章节文件名，以后的参数为: 章节选项文案 章节编号,多个章节选项以此类推`);
@@ -33,14 +36,9 @@ const generateChapter = (cmd_input) => {
     chapterBody.push(o);
   }
 
-  let chaptersJSONText = fs.readFileSync(jsChapterJSONPath, {
-    encoding: 'utf8'
-  });
+  chapters[chapter_name] = chapterBody;
 
-  let chaptersJSON = JSON.parse(chaptersJSONText);
-  chaptersJSON[chapter_name] = chapterBody;
-
-  chaptersJSONText = JSON.stringify(chaptersJSON, null, autoIntent ? 4 : 0);
+  let chaptersJSONText = JSON.stringify(chapters, null, autoIntent ? 4 : 0);
   fs.writeFileSync(jsChapterJSONPath, chaptersJSONText);
   fs.writeFileSync(jsChapterFilePath, `const chapters = ${chaptersJSONText}`);
 
