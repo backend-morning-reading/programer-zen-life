7bf756b32b8e5d41f15d7bec619ae79c6915df57
chenjinya <jinyachen@gmail.com>
2019-07-08 13:22:31 +0800

    new noval

--- docs/scripts/generator.js
+++ docs/scripts/generator.js
@@ -3,10 +3,22 @@
 //生成章节
 
 const fs = require('fs');
+const jsChapterFilePath = __dirname + '/chapters.js';
+const jsChapterJSONPath = __dirname + '/chapters.json';
+const chapters = require(jsChapterJSONPath);
+const autoIntent = true;
+
 const generateChapter = (cmd_input) => {
-  const jsChapterFilePath = './scripts/chapters.js';
-  const jsChapterJSONPath = './scripts/chapters.json';
-  const autoIntent = true;
+
+  fs.writeFileSync(jsChapterJSONPath + '.last-backup', JSON.stringify(chapters, null, autoIntent ? 4 : 0));
+  if (cmd_input.length > 0 && isNaN(cmd_input[0])) {
+    let maxChapterNum = 0;
+    for (let _i in chapters) {
+      if (_i - maxChapterNum > 0) maxChapterNum = _i;
+    }
+    cmd_input.unshift((parseInt(maxChapterNum) + 1).toString());
+  }
+
   if (!cmd_input || 0 === cmd_input.length % 2) {
     console.error(`❌ Generate fail!  第一个参数为章节文件名，以后的参数为: 章节选项文案 章节编号,多个章节选项以此类推`);
     process.exit(0);
@@ -14,6 +26,9 @@ const generateChapter = (cmd_input) => {
 
   let chapter_file_name = cmd_input[0];
 
+  if (-1 == chapter_file_name.indexOf('.md')) {
+    chapter_file_name = chapter_file_name + ".md";
+  }
   let chapter_options = [];
   for (let i = 1; cmd_input[i]; i += 2) {
     chapter_options.push([cmd_input[i], cmd_input[i + 1]]);
@@ -29,18 +44,17 @@ const generateChapter = (cmd_input) => {
   let chapterBody = [
     [fileContent]
   ];
+
+  console.log("+current: " + chapter_name);
   for (let o of chapter_options) {
     chapterBody.push(o);
+    console.log(" |--next: " + o[1] + ' ' + o[0]);
+
   }
 
-  let chaptersJSONText = fs.readFileSync(jsChapterJSONPath, {
-    encoding: 'utf8'
-  });
+  chapters[chapter_name] = chapterBody;
 
-  let chaptersJSON = JSON.parse(chaptersJSONText);
-  chaptersJSON[chapter_name] = chapterBody;
-
-  chaptersJSONText = JSON.stringify(chaptersJSON, null, autoIntent ? 4 : 0);
+  let chaptersJSONText = JSON.stringify(chapters, null, autoIntent ? 4 : 0);
   fs.writeFileSync(jsChapterJSONPath, chaptersJSONText);
   fs.writeFileSync(jsChapterFilePath, `const chapters = ${chaptersJSONText}`);
 
